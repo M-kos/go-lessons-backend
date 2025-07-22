c20e4c5a0e73fb6268c84ba23abd53427037f703
morrowsol <morrowsol@MacBook-Pro-morrowsol.local>
2025-07-22 23:26:24 +0300

    (feat): add get by email handler

--- internal/controller/v1/user_controller.go
+++ internal/controller/v1/user_controller.go
@@ -5,12 +5,14 @@ import (
 	"encoding/json"
 	"log/slog"
 	"net/http"
+	"time"
 
 	"github.com/meetmorrowsolonmars/education-pet-project/internal/domain"
 )
 
 type UserStore interface {
 	Create(ctx context.Context, user domain.User) (domain.User, error)
+	GetByEmail(ctx context.Context, email string) (domain.User, error)
 }
 
 type UserController struct {
@@ -72,6 +74,63 @@ func (c *UserController) CreateUser(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type User struct {
+	ID         int64     `json:"id"`
+	Email      string    `json:"email"`
+	FullName   string    `json:"full_name"`
+	CreateTime time.Time `json:"create_time"`
+}
+
+type GetByEmailResponse struct {
+	User
+}
+
+func (c *UserController) GetByEmail(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	const pathKey = "email"
+
+	email := r.PathValue(pathKey)
+	if email == "" {
+		slog.Error("Get user by email", slog.String("error", "email is empty"))
+
+		err := WriteErrorResponse(w, http.StatusBadRequest, "Email is required")
+		if err != nil {
+			slog.Error("Write error response", slog.String("error", err.Error()))
+		}
+
+		return
+	}
+
+	user, err := c.userStore.GetByEmail(r.Context(), email)
+	if err != nil {
+		slog.Error("Get user by email", slog.String("error", err.Error()))
+
+		err = WriteErrorResponse(w, http.StatusInternalServerError, "Get user by email error")
+		if err != nil {
+			slog.Error("Write error response", slog.String("error", err.Error()))
+		}
+
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	err = json.NewEncoder(w).Encode(GetByEmailResponse{
+		User: User{
+			ID:         user.ID,
+			Email:      user.Email,
+			FullName:   user.FullName,
+			CreateTime: user.CreateTime,
+		},
+	})
+	if err != nil {
+		slog.Error("Write get user by email response", slog.String("error", err.Error()))
+	}
+}
+
 func (c *UserController) Register(mux *http.ServeMux) {
+	// TODO: Show different ways to design API (REST, RPC).
 	mux.HandleFunc("POST /v1/users", c.CreateUser)
+	mux.HandleFunc("GET /v1/users/{email}", c.GetByEmail)
 }
